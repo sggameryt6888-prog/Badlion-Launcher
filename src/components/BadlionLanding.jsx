bc02a03d870c5cfa82edb0f7dfbce109886a26b2
sggameryt6888-prog <sggameryt6888@gmail.com>
2026-04-04 14:14:22 +0530

    Create BadlionLanding component with esports aesthetic

---
+++ src/components/BadlionLanding.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './BadlionLanding.css';
+
+const BadlionLanding = () => {
+  return (
+    <div className="landing">
+      <h1 className="title">Welcome to Badlion Client</h1>
+      <p className="description">Experience the ultimate gaming platform with enhanced performance and stunning visuals.</p>
+      <button className="cta-button">Get Started</button>
+      <div className="particles"></div>
+    </div>
+  );
+};
+
+export default BadlionLanding;
